a705c12aef60f7970c8ed2f3ee26e8f9f20725f1
gatevin <gatevin@outlook.com>
2019-07-23 00:14:42 +0800

    update

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/proxy"
 	"github.com/satori/go.uuid"
-	"matou-sakura/config"
+	//"matou-sakura/config"
 	"fmt"
 	"time"
 	//"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"math/rand"
 	"os"
 )
 
@@ -18,7 +19,7 @@ type ImageDownloader struct{
 
 }
 
-func (imgDL *ImageDownloader) Download(imgUrl string, done chan struct{}) error {
+func (imgDL *ImageDownloader) Download(imgUrl string) error {
 	//fmt.Println("To download url: ", imgUrl)
 	uuid, _ := uuid.NewV4()
 	saveDir := "./download/"
@@ -31,14 +32,12 @@ func (imgDL *ImageDownloader) Download(imgUrl string, done chan struct{}) error 
 	resp, err := client.Get(imgUrl)
 	if err != nil {
 		fmt.Println("Http error when download image : ", imgUrl, "\nError msg: ", err, " UUID: ", uuid)
-		<-done
 		return nil
 	 }
 	defer resp.Body.Close()
 	file, err_file := os.Create(imgName)
 	if err_file != nil {
 		fmt.Println("Error when create file: ", imgName, "\nError msg: ", err_file, " UUID: ", uuid)
-		<-done
 		return nil
 	}
 	defer file.Close()
@@ -46,12 +45,10 @@ func (imgDL *ImageDownloader) Download(imgUrl string, done chan struct{}) error 
 	content, err2 := ioutil.ReadAll(resp.Body)
 	if err2 != nil {
 		fmt.Println("Error in ioutil : ", imgName, "\nError msg: ", err2, " UUID: ", uuid)
-		<-done
 		return nil
 	}
 	file.Write(content)
 	//fmt.Println("download img ", imgUrl, " finish")
-	<-done
 	return nil
 }
 
@@ -61,19 +58,31 @@ type YandeCollector struct {
 	MainUrl string
 	SearchKeyWord string
 	YandeColly *colly.Collector
+	task chan string
 	done chan struct{}
 }
 
 var YandeHandler = &YandeCollector{}
 
 func (yc *YandeCollector) Prepare(keyword string) error {
-	yc.done = make(chan struct{}, 20)
+	yc.task = make(chan string, 20)
+	yc.done = make(chan struct{})
+	for i := 0; i < 3; i++ {
+		go func(){
+			for imgUrl := range yc.task {
+				imageDownloader.Download(imgUrl)
+				num := rand.Int31n(5)
+				time.Sleep(time.Duration(num)*time.Second)
+			}
+			yc.done <- struct{}{}
+		}()
+	}
 	yc.MainUrl = "https://yande.re"
 	yc.SearchKeyWord = keyword
 	yc.YandeColly = colly.NewCollector(
 		colly.AllowedDomains("yande.re"),
-		colly.MaxDepth(3),
-		//colly.Async(true),
+		//colly.MaxDepth(2),
+		colly.Async(true),
 	)
 	rp, err := proxy.RoundRobinProxySwitcher("http://127.0.0.1:1080")
 	if err != nil {
@@ -83,8 +92,8 @@ func (yc *YandeCollector) Prepare(keyword string) error {
 	yc.YandeColly.SetProxyFunc(rp)
 	yc.YandeColly.Limit(&colly.LimitRule{
 		DomainGlob: "*",
-		//Parallelism: 4,
-		RandomDelay: 4*time.Second,
+		Parallelism: 2,
+		RandomDelay: 5*time.Second,
 	})
 	yc.YandeColly.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting: ", r.URL)
@@ -102,8 +111,9 @@ func (yc *YandeCollector) Prepare(keyword string) error {
 	yc.YandeColly.OnHTML("a[class^=directlink][class$=largeimg][href]", func(e *colly.HTMLElement) {
 		// 前往大图进行下载
 		downloadUrl := e.Attr("href")
-		yc.done <- struct{}{}
-		go imageDownloader.Download(downloadUrl, yc.done)
+		yc.task <- downloadUrl
+		//go imageDownloader.Download(downloadUrl, yc.task)
+
 	})
 	yc.YandeColly.OnHTML("a[class=next_page][rel=next][href]", func(e *colly.HTMLElement) {
 		// 下一页
@@ -121,23 +131,22 @@ func (yc *YandeCollector) Prepare(keyword string) error {
 }
 
 func (yc *YandeCollector) Start() {
-	searchUrl := "https://yande.re/post?tags=" + yc.SearchKeyWord + "+"
+	searchUrl := "https://yande.re/post?page=9&tags=" + yc.SearchKeyWord + "+"
 	yc.YandeColly.Visit(searchUrl)
 	yc.YandeColly.Wait()
 }
 
 func (yc *YandeCollector) End() {
-	for {
-		fmt.Println("YC length: ", len(yc.done))
-		if len(yc.done) == 0 { break }
+	close(yc.task)
+	for i := 0; i < 3; i++ {
+		<-yc.done
 	}
-
 }
 
 func main() {
 	searchKeyWord := "matou_sakura"
-	confVip := config.LoadConfig()
-	fmt.Println(confVip.GetString("proxy.addr"))
+	//confVip := config.LoadConfig()
+	//fmt.Println(confVip.GetString("proxy.addr"))
 	YandeHandler.Prepare(searchKeyWord)
 	YandeHandler.Start()
 	YandeHandler.End()
